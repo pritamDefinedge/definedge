2c8398484c6c9b5402264ba7f2e6d24cbc235c63
NeginSal <neginsaljooghi@gmail.com>
2023-08-04 16:49:24 +0330

    added:materialicon,pinia,Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-red-700">
+      hello world
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import { createPinia } from 'pinia'
 
-const app = createApp(App)
+const app = createApp(App).use(createPinia())
 
 app.use(router)
 
