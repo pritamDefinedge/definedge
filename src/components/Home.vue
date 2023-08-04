bcae67c0527ebf337157a0626ca68ea6489dda8e
NeginSal <neginsaljooghi@gmail.com>
2023-08-04 21:47:45 +0330

    added:PostList component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
-
     <!---loading-->
-    <div v-if="UserStore.loading">Loading users ...</div>
+    <!-- <div v-if="UserStore.loading">Loading users ...</div>
     <div v-for="user in UserStore.users" :key="user.id">
       <router-link :to="`/user/${user.id}`">
         <p>{{ user.name }}</p>
       </router-link>
     </div>
-    <AddUser />
+    <AddUser /> -->
+    <PostList/>
   </div>
 </template>
 
 <script>
-import { useUserStore } from "../stores/UserStore";
-import AddUser from "./AddUser.vue";
-import UserDetail from "./UserDetail.vue";
+import PostList from "./PostList.vue";
+// import { useUserStore } from "../stores/UserStore";
+// import AddUser from "./AddUser.vue";
+// import UserDetail from "./UserDetail.vue";
 
 export default {
-  setup() {
-    const UserStore = useUserStore();
-    //fetch users
-    UserStore.getUsers();
-    return { UserStore };
-  },
-  components: { AddUser, UserDetail }
+  components: { PostList }
+  // setup() {
+  //   const UserStore = useUserStore();
+  //   //fetch users
+  //   UserStore.getUsers();
+  //   return { UserStore };
+  // },
+  // components: { AddUser, UserDetail }
 };
 </script>
 
---
+++ src/components/PostList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!---loading-->
+    <div v-if="PostStore.loading">Loading users ...</div>
+    <div v-for="post in PostStore.posts" :key="post.id">
+      <router-link :to="`/user/${post.id}`">
+        <p>{{ post.title }}</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { usePostStore } from "../stores/PostStore";
+export default {
+  setup() {
+    const PostStore = usePostStore();
+
+    //fetch posts
+    PostStore.getPosts();
+    return { PostStore };
+  },
+}
+</script>
